55fcf4cd1811f6c1e045b3f5d2fe9da9dbbfa1d4
Freez <114873472+Freez00@users.noreply.github.com>
2023-03-14 17:20:13 +0200

    initial commit

---
+++ Tasks-Wait_WaitAll.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace izpit14032023
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+        static List<Book> books = new List<Book>();
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(AddBooksToList);
+            Task task2 = new Task(FilterCheapBooks);
+            Task task3 = new Task(PrintBookList);
+
+            task1.Start();
+            task1.Wait();
+
+            task2.Start();
+            Task.WaitAll(task2, task1);
+
+            task3.Start();
+
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) ;
+        }
+        static void AddBooksToList()
+        {
+            Console.WriteLine("Currently running task id - {0}   [AddBooksToList]", Task.CurrentId);
+            for (int i = 0; i < 5; i++)
+            {
+                Book temp_book = new Book() { Title = "Book_" + i, Price = rnd.Next(50) };
+                books.Add(temp_book);
+                Console.WriteLine("[{0}] A book has been added to the list.", Task.CurrentId);
+            }
+        }
+        static void FilterCheapBooks()
+        {
+            Console.WriteLine("Currently running task id - {0}   [FilterCheapBooks]", Task.CurrentId);
+            for(int i = 0; i < books.Count; i++)
+            {
+                if (books[i].Price < 25)
+                {
+                    books.Remove(books[i]);
+                    Console.WriteLine("[{0}] A book has been removed from the list.", Task.CurrentId);
+                    i--;
+                }
+            }
+        }
+
+        static void PrintBookList()
+        {
+            Console.WriteLine("Currently running task id - {0}   [PrintBookList]", Task.CurrentId);
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+    class Book
+    {
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} costs {Price}";
+        }
+    }
+}
